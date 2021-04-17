9a51ef138f7b903a85ef1b60d4e6d2afa57902c0
DaniCristante <daniel.cristante@gmail.com>
2021-04-17 17:22:33 +0200

    add home component and 404 component

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,19 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
-
 import { PassangerDashboardModule } from './passanger-dashboard/passanger-dashboard.module';
 
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home.component';
+import { NotFoundComponent } from './base-components/not-found.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
     CommonModule,
-    PassangerDashboardModule
+    PassangerDashboardModule,
+    RouterModule.forRoot(routes)
   ],
   bootstrap: [AppComponent]
 })
