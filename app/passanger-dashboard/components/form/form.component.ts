2901df69333753989cb073f7de528a408012a34d
DaniCristante <daniel.cristante@gmail.com>
2021-04-17 02:20:29 +0200

    added options

--- app/passanger-dashboard/components/form/form.component.ts
+++ app/passanger-dashboard/components/form/form.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from "@angular/core";
 import { Passanger } from '../../models/passanger.interface';
+import { BaggageInterface } from '../../models/baggage.interface';
 
 @Component({
     selector: 'passanger-form',
@@ -10,6 +11,26 @@ import { Passanger } from '../../models/passanger.interface';
 export class PassangerFormComponent {
     @Input()
     detail: Passanger;
+
+    baggage: BaggageInterface[] = 
+    [
+        {
+            key: 'none',
+            value: 'No Bagagge'
+        },
+        {
+            key: 'hand',
+            value: 'Hand Bagagge'
+        },
+        {
+            key: 'hold',
+            value: 'Hold Bagagge'
+        },
+        {
+            key: 'hand-hold',
+            value: 'Hand and hold bagagge'
+        },
+    ];
 
     toggleCheckIn(checkedIn: boolean) {
         if (checkedIn) {
